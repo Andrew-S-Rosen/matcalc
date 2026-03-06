5ba8dcc3f0572d2d253f0f0ffe3f3bebb810ef7f
Andrew Rosen <asrosen93@gmail.com>
2026-03-06 10:10:31 -0500

    change fmax

--- src/matcalc/_qha.py
+++ src/matcalc/_qha.py
@@ -82,7 +82,7 @@ class QHACalc(PropCalc):
         t_step: float = 10,
         t_max: float = 1000,
         t_min: float = 0,
-        fmax: float = 0.1,
+        fmax: float = 1e-5,
         optimizer: str = "FIRE",
         eos: str = "vinet",
         relax_structure: bool = True,
